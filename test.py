59e16cd4134605ae99c5237abea43e5e35b9f5f9
Niels Herrmann <nielsarik1@gmail.com>
2019-10-15 10:44:40 +0200

    ny fil

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-dfdfdfdf
+import pygame
+pygame.init()
+window = pygame.display.set_mode((1000, 500))
+pygame.display.set_caption("My First Python game")
+
+x = 50
+y = 50
+hight = 60
+Width = 40
+vel = 5
+
+run = True
+while run:
+      pygame.time.delay(100)
+      for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+          run = False
+
+pygame.quit()
+
+
